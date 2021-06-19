b010e80450f82181440b5e71b5876a9c0ac8f310
Michael Bernard <38008520+michaelbnd@users.noreply.github.com>
2021-06-19 05:34:02 +0200

    Removed Fonts

--- App.tsx
+++ App.tsx
@@ -2,19 +2,12 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
-import useCachedResources from './hooks/useCachedResources';
 import Navigation from './navigation';
 
 export default function App() {
-  const isLoadingComplete = useCachedResources();
-
-  if (!isLoadingComplete) {
-    return null;
-  } else {
-    return (
-      <SafeAreaProvider>
-        <Navigation/>
-      </SafeAreaProvider>
-    );
-  }
+  return (
+    <SafeAreaProvider>
+      <Navigation/>
+    </SafeAreaProvider>
+  );
 }
