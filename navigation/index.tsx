62800a18aeb7071ee98d43427466f2b20616caf1
Michael Bernard <38008520+michaelbnd@users.noreply.github.com>
2021-06-19 05:36:15 +0200

    Removed Linking

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -6,16 +6,13 @@
 import { NavigationContainer, DefaultTheme, DarkTheme } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import * as React from 'react';
-import { ColorSchemeName } from 'react-native';
 
 import { RootStackParamList } from '../types';
 import BottomTabNavigator from './BottomTabNavigator';
-import LinkingConfiguration from './LinkingConfiguration';
 
 export default function Navigation() {
   return (
-    <NavigationContainer
-      linking={LinkingConfiguration}>
+    <NavigationContainer>
       <RootNavigator />
     </NavigationContainer>
   );
